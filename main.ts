846959cc16f6e1ade65b6f11200f25a41627853d
HectorBergman <113025156+HectorBergman@users.noreply.github.com>
2022-09-08 11:04:57 +0200

    Fixade koden!!

--- main.ts
+++ main.ts
@@ -2,22 +2,42 @@ let Håll = 0
 let Hållbörjan = input.compassHeading()
 let andrahållet = 0
 basic.forever(function () {
-    Håll = input.compassHeading() - Hållbörjan + 90
+    Håll = input.compassHeading()
 })
 basic.forever(function () {
     if (andrahållet == 0) {
-        if (Håll > Hållbörjan + 135 && Håll < Hållbörjan + 225) {
-            basic.showIcon(IconNames.Happy)
-            andrahållet = 1
-        } else {
-            basic.showIcon(IconNames.Sad)
+        if (Hållbörjan + 135 < Hållbörjan + 225) {
+            if (Håll > Hållbörjan + 135 && Håll < Hållbörjan + 225) {
+                basic.showIcon(IconNames.Happy)
+                andrahållet = 1
+            } else {
+                basic.showIcon(IconNames.Sad)
+            }
+            if (Hållbörjan + 135 > Hållbörjan + 225) {
+                if (Håll < Hållbörjan + 135 && Håll > Hållbörjan + 225) {
+                    basic.showIcon(IconNames.Asleep)
+                    andrahållet = 0
+                } else {
+                    basic.showIcon(IconNames.Angry)
+                }
+            }
         }
     } else {
-        if (Håll > Hållbörjan - 45 && Håll < Hållbörjan + 45) {
-            basic.showIcon(IconNames.Sad)
-            andrahållet = 0
-        } else {
-            basic.showIcon(IconNames.Happy)
+        if (Hållbörjan + 135 < Hållbörjan + 225) {
+            if (Håll > Hållbörjan - 45 && Håll < Hållbörjan + 45) {
+                basic.showIcon(IconNames.Sad)
+                andrahållet = 0
+            } else {
+                basic.showIcon(IconNames.Happy)
+            }
+        }
+        if (Hållbörjan + 135 > Hållbörjan + 225) {
+            if (Håll < Hållbörjan - 45 && Håll > Hållbörjan + 45) {
+                basic.showIcon(IconNames.Angry)
+                andrahållet = 0
+            } else {
+                basic.showIcon(IconNames.Asleep)
+            }
         }
     }
 })
